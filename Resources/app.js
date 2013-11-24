1a999f8a6840d695cc5e02fa6202d30bd76a48a3
Allen Chun <allen.chun@hotmail.com>
2013-11-24 22:56:28 +0800

    UI samples for Titanium

--- Resources/app.js
+++ Resources/app.js
@@ -5,6 +5,19 @@ var win = Titanium.UI.createWindow({
 	exitOnClose: true // For Android, have the app close on the exiting of the windows
 });
 
+
+var win2 = Titanium.UI.createWindow({
+	title: "Our window titile",
+	backgroundColor: "#FFFFFF",
+	exitOnClose: true, // For Android, have the app close on the exiting of the windows
+	url: "win2.js"
+});
+
+
+// getting the OS name (iPhone, iPad, android, mobileweb)
+var PLATFORM = Titanium.Platform.osname;
+
+
 //create a view
 var view1 = Titanium.UI.createView({
 	height: 50, //height in pixels
@@ -24,7 +37,7 @@ var label1 = Titanium.UI.createLabel({
 	textAlign: "center",
 	color: "#000000",
 	font: {fontSize: 36, fontstyle: "italic"},
-	backgroundColor: "#363F45"
+	backgroundColor: "#FFFFFF"
 });
 
 
@@ -36,7 +49,63 @@ var imageView1 = Titanium.UI.createImageView({
 	top: 100
 });
 
+
+//Adding an event when image was clicked
+imageView1.addEventListener("click", function(e){
+	var toast = Titanium.UI.createNotification({
+		duration: 2000,
+		message: "I am Iron Man!"
+	});
+	toast.show();
+});
+
+// creating a native button
+var button1 = Titanium.UI.createButton({
+	title: "What OS is this?",
+	width: "25%",
+	height: "10%",
+	top: 300,
+	id: "Simple Button"
+});
+
+// creating a click event that fires an alert dialog
+button1.addEventListener("click", function(e){
+	//alert(e.source.id + " fired a " + e.type + " event");
+	alert(PLATFORM);
+});
+
+//creating a slider
+var slider1 = Titanium.UI.createSlider({
+	height: "auto",
+	width: 300,
+	top: 450,
+	min: 0, // setting the min value for the slider (needed for Android)
+	max: 100 // setting the max value for the slider (needed for Android)
+});
+
+
+//creating a slider
+var switch1 = Titanium.UI.createSwitch({
+	top: 500,
+	value: true // set the initial value of the switch, true or false	
+});
+
+//creating a switch
+switch1.addEventListener("change", function(e){
+	label1.text = e.value;
+});
+
+
+//Displays the value to the label 
+slider1.addEventListener("change", function(e){
+	label1.text = Math.round(e.value);
+});
+
+
 win.add(view1); // adding the view1 variable to the window
 win.add(label1); // adding the label to the window
 win.add(imageView1); // adding the imageView to the window
+win.add(button1); // adding the button to the window
+win.add(slider1); // adding the slider to the window
+win.add(switch1); // adding the switch to the window
 win.open(); // opening the window
